f1ac703c65fac2ab80be9252b721e576d011387b
paramvirSinghBhumra <paramvirsbhumra@gmail.com>
2020-07-20 01:59:02 -0500

    edited the text

--- main.py
+++ main.py
@@ -49,25 +49,27 @@ def send_email(firstname="null", lastname='null', receiver_email='personaldevelo
       <body>
         <p>
         Hello Parents and Campers!
-
+        <br><br>
         We hope you are staying safe and healthy during these crazy times. We understand a lot has changed, school, classes, life in general - however, we hope we can make it a little better for you!
-
+        <br><br>
         We are happy to announce we will be hosting our 6th ANNUAL CAMP this upcoming August (Aug 3-7) VIRTUALLY. Due to social distancing measures, we wanted to share this opportunity to you all. Children of all skill levels are welcome, where Colorado Bhangra's own dancers will be instructing and leading classes over zoom. Then, there will be a final performance held at the end of the week to be showcased to all friends and family.
-
+        <br><br>
         Attached is a form that needs to be completed by JULY 29th to be admitted into camp.
-
+        <br><br>
         Pricing:
         $45 per child, ($35 per child for siblings) due in full before the 31st of July. If you sign up before the 27th, you'll get a 10% DISCOUNT! 
-
+        <br><br>
         There will be a confirmation email of your submission and instructions of payment will be listed in the email.
+        <br>
+        <br>
+        <br>
 
-
-
-        If you have general questions, please contact mailto:coloradobhangrainstitute@gmail.com.  
-
+        If you have general questions, please contact <a href="mailto:coloradobhangrainstitute@gmail.com">coloradobhangrainstitute@gmail.com</a>.  
+        <br><br>
         For one on one questions, please contact Captain Vriti Seth at tel:(303)-809-0300 or Captian Ria Sunil at tel:(303)-887-7103
+        <br><br>
         </p>
-           <iframe src="//https://docs.google.com/forms/d/e/1FAIpQLSceGkMaS4G8tHPHBq2IK09jDTV8IIcrLeQK_QaTu2hZc_tH4w/viewform?embedded=true" width="640" height="410" frameborder="0" marginheight="0" marginwidth="0"><a href="https://forms.gle/87JJb8P98fUqro6C9">Here's the signup form</a></iframe>
+           <iframe src="//https://docs.google.com/forms/d/e/1FAIpQLSceGkMaS4G8tHPHBq2IK09jDTV8IIcrLeQK_QaTu2hZc_tH4w/viewform?embedded=true" width="640" height="410" frameborder="0" marginheight="0" marginwidth="0"><a href="https://forms.gle/87JJb8P98fUqro6C9" style="color:red">SIGN UP FORM</a></iframe>
       </body>
     </html>
     """
